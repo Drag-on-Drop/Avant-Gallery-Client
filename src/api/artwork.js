3bfee9c8267dc0ae1bd608ced3766abd968c37ea
stevendo <stevendo@me.com>
2020-07-15 17:02:15 -0400

    added IA's artwork api to artwork.js

---
+++ src/api/artwork.js
@@ -0,0 +1,41 @@
+import apiUrl from '../apiConfig'
+import axios from 'axios'
+export const addArtwork = (formData, user) => {
+  return axios({
+    method: 'POST',
+    url: apiUrl + '/artworks',
+    headers: {
+      'Authorization': `Token token=${user.token}`
+    },
+    data: formData
+  })
+}
+export const showArtwork = (formData, user) => {
+  return axios({
+    url: apiUrl + '/artworks',
+    method: 'GET',
+    headers: {
+      'Authorization': `Token token=${user.token}`
+    },
+    data: formData
+  })
+}
+export const deleteArtwork = (formData, user) => {
+  return axios({
+    url: apiUrl + '/artworks',
+    method: 'DELETE',
+    headers: {
+      'Authorization': `Token token=${user.token}`
+    }
+  })
+}
+export const editArtwork = (formData, user) => {
+  return axios({
+    url: apiUrl + '/artworks',
+    method: 'PATCH',
+    headers: {
+      'Authorization': `Token token=${user.token}`
+    },
+    data: formData
+  })
+}
