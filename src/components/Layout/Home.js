0f8ecb1736773240375726f97c66f1ea961846e2
stevendo <stevendo@me.com>
2020-07-15 16:38:36 -0400

    This is everything.

--- src/components/Layout/Home.js
+++ src/components/Layout/Home.js
@@ -1,6 +1,6 @@
 import React from 'react'
-import Layout from '../Layout/Layout'
-import MainPicture from '../MainPicture/MainPicture'
+import Layout from './Layout'
+import MainPicture from './MainPicture'
 
 const Home = () => (
   <Layout>
