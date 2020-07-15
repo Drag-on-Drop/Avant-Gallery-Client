14e8b6c5c1f1ea2be536ed027f5cbd50930836a9
stevendo <stevendo@me.com>
2020-07-15 14:59:45 -0400

    sorry i made new component on dev branch edits on home.js and created MainPicture.js

--- src/components/Layout/Home.js
+++ src/components/Layout/Home.js
@@ -1,9 +1,11 @@
 import React from 'react'
-import Layout from './Layout'
+import Layout from '../Layout/Layout'
+import MainPicture from '../MainPicture/MainPicture'
 
 const Home = () => (
   <Layout>
     <h4>Home</h4>
+    <MainPicture></MainPicture>
   </Layout>
 )
 
