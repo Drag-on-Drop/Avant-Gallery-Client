1cec037caac54c7c2b1ea3dd48e71f72afb24c73
stevendo <stevendo@me.com>
2020-07-17 22:42:55 -0400

    footer justified right with a little space

--- src/components/Layout/Footer.js
+++ src/components/Layout/Footer.js
@@ -3,7 +3,7 @@ import React from 'react'
 const Footer = () => (
   <div className="footer">
     <hr/>
-    <p style={{ color: 'grey', textAlign: 'left' }}>Developed by Isaac A., Will A., Steven D., Jason F.</p>
+    <p style={{ color: 'grey', textAlign: 'right' }}>Developed by Isaac A., Will A., Steven D., Jason F.&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;</p>
   </div>
 )
 
